fe6fa65c78a64fa6254dfd17243fc98fcdeb6177
wendy02062001 <wendy.nguyen262001@gmail.com>
2022-04-13 22:30:41 +0700

    init login and reg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Switch, Router } from "react-router-dom";
+import DangKy from "./pages/DangKy/DangKy";
+import DangNhap from "./pages/DangNhap/DangNhap";
 import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
 import { LoginTemplate } from "./templates/LoginTemplate";
 import { history } from "./utils/setting";
 function App() {
@@ -9,8 +9,8 @@ function App() {
     <Router history={history}>
       <Switch>
         <Route exact path={"/home"} component={Home} />
-        <LoginTemplate exact path={"/login"} component={Login} />
-        <LoginTemplate exact path={"/register"} component={Register} />
+        <LoginTemplate exact path={"/dang-nhap"} component={DangNhap} />
+        <LoginTemplate exact path={"/dang-ky"} component={DangKy} />
         <Route exact path={"/"} component={Home} />
       </Switch>
     </Router>
---
+++ src/pages/DangNhap/DangNhap.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DangNhap() {
+  return <div>DangNhap</div>;
+}
