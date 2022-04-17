58cedc215a38793b3c894a630181f58785046851
unknown <thangdoanplus@gmail.com>
2022-04-17 23:59:56 +0700

    Created HeaderHome and FooterHome component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { BrowserRouter, Route, Switch, Router } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import { history } from "./utils/setting";
+import { BrowserRouter, Route, Switch, Router } from 'react-router-dom';
+import FooterHome from './components/FooterHome/FooterHome';
+import HeaderHome from './components/HeaderHome/HeaderHome';
+import Home from './pages/Home/Home';
+import { history } from './utils/setting';
 function App() {
   return (
     <Router history={history}>
+      <HeaderHome />
       <Switch>
-        <Route exact path={"/home"} component={Home} />
-        <Route exact path={"/"} component={Home} />
+        <Route exact path={'/home'} component={Home} />
+        <Route exact path={'/'} component={Home} />
       </Switch>
+      <FooterHome />
     </Router>
   );
 }
---
+++ src/components/HeaderHome/HeaderHome.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+export default function HeaderHome() {
+  return (
+    <nav className='navbar navbar-expand-sm navbar-dark bg-white'>
+      <a className='navbar-brand' href='#'>
+        <img src='./images/MIN-OP1.png' style={{ height: '50px' }} />
+      </a>
+      <button
+        className='navbar-toggler d-lg-none'
+        type='button'
+        data-toggle='collapse'
+        data-target='#collapsibleNavId'
+        aria-controls='collapsibleNavId'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon' />
+      </button>
+      <div className='collapse navbar-collapse d-flex' id='collapsibleNavId'>
+        <div className='col-4'>
+          <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+            <li className='nav-item dropdown'>
+              <a
+                className='nav-link dropdown-toggle'
+                href='#'
+                id='dropdownId'
+                data-toggle='dropdown'
+                aria-haspopup='true'
+                aria-expanded='false'
+              >
+                Danh mục khoá học
+              </a>
+              <div className='dropdown-menu' aria-labelledby='dropdownId'>
+                <a className='dropdown-item' href='#'>
+                  Action 1
+                </a>
+                <a className='dropdown-item' href='#'>
+                  Action 2
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className='col-4'>
+          <form className='my-2 my-lg-0'>
+            <input
+              className='form-control mr-sm-2'
+              type='text'
+              placeholder='Tìm khoá học'
+            />
+          </form>
+        </div>
+        <div className='col-4'>
+          <button
+            className='btn default-button my-2 mr-4 my-sm-0'
+            type='button'
+          >
+            Đăng kí
+          </button>
+          <button className='btn default-button my-2 my-sm-0' type='button'>
+            Đăng nhập
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
